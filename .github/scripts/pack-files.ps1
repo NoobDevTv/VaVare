9c579429ce1097a01bc2f0288dcfe95624e56d5a
Maximilian Krüger <Marcus.Aurelius@gallimathias.de>
2021-09-04 14:49:47 +0200

    Use the other enable syntax
    * Use !#USE_Nuget instead #NUGET# to follow more the rest patterns

--- .github/scripts/pack-files.ps1
+++ .github/scripts/pack-files.ps1
@@ -4,7 +4,8 @@ if(!(Test-Path $path)){
     exit;
 }
 
-Get-ChildItem -Recurse -Path $path -Include *.csproj | Select-String "#NUGET#" -List | Select-Object Path | ForEach-Object {    
+$enable = "!#USE_"+"Nuget"
+Get-ChildItem -Recurse -Path $path -Include *.csproj | Select-String $enable -List | Select-Object Path | ForEach-Object {    
     Write-Host "Pack Project" $_.Path
     dotnet pack $_.Path -c Release --no-build 
 }
