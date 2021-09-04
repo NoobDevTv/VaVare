0654f3f1b90f366c2e7365d7a8638ef42df917ec
Maximilian Krüger <Marcus.Aurelius@gallimathias.de>
2021-09-04 15:40:59 +0200

    Try to use Parameter name
    To be more safe on linux try to use the Parameter name isntead position parsing

--- .github/scripts/replace.ps1
+++ .github/scripts/replace.ps1
@@ -11,8 +11,7 @@ Write-Host "Use $name"
 $childItems = Get-ChildItem -Recurse -Path $path
 $length = $childItems.Length
 Write-Host "Found $length files"
-Write-Host ($childItems | Select-String $enable -List)
-$selection = $childItems | Select-String $enable -List
+$selection = $childItems | Select-String -Pattern $enable -List
 $length = $selection.Length
 Write-Host "Select $length files"
 $paths = $selection | Select-Object Path
