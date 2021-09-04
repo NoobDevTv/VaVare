2efe5b43504fcb37c1e9175c04e3adc314bb816b
Maximilian Krüger <Marcus.Aurelius@gallimathias.de>
2021-09-04 14:27:16 +0200

    Extract litgit logic in seperate file
    * Because of some issues extract the hole lit git parsing logic to a seprate script file

---
+++ .github/scripts/parse-template.ps1
@@ -0,0 +1,11 @@
+param($path, $litgit, $config)
+
+if(!(Test-Path $path)){
+    exit;
+}
+
+$enable = "!#USE_"+"LitGit"
+Get-ChildItem -Recurse -Path $path | Select-String $enable -List | Select-Object Path | ForEach-Object {    
+    Write-Host "Parse template $_.Path"
+    & $litgit -c $config -t $_.Path
+}
